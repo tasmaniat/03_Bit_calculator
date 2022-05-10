72ce3674cd5b21728f909438ed6c42ebbaf5330d
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2022-05-10 12:19:46 +1200

    added code

--- 02_input_checker_v2.py
+++ 02_input_checker_v2.py
@@ -1,15 +1,34 @@
 #checks user choice is "integer", "text" or "image"
 def user_choice ():
 
+    # Lists of valid responses
+    text_ok = ["text", "t", "txt"]
+    integer_ok = ["integer", "int", "#" "number"]
+    image_ok = ["image", "img", "pix", "picture", "pic", "p"]
+
     valid = False
     while not valid:
 
         # ask user for choice and change responce to lowercase
         response = input("File type (integer / text / image): ").lower()
 
-        # if they choose "t" or "text", return "text"
-        if response == "text"or response == "t":
+        # Checks for valid respondse and returns text, integer or image
+
+        if response in text_ok:
             return "text"
+
+        elif response in integer_ok:
+            return "integer"
+
+        elif response in image_ok:
+            return "image"
+        
+        elif response == "i":
+            want_interger = input("press <enter> for an integer or any key for an image: ")
+            if want_interger == "":
+                return "integer"
+            else:
+                return "image"
 
         else:
             # if responce is not valid, output an error
@@ -18,7 +37,9 @@ def user_choice ():
 
 
 # Main routine goes here
-data_type = user_choice()
-print("you chose", data_type)
+keep_going = ""
+while keep_going == "":
+    data_type = user_choice()
+    print("you chose", data_type)
 
-print()
+    print()
