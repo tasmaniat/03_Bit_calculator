8d289eafe952177bea4a342d74adf8ae3f0dd632
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2022-05-16 13:50:45 +1200

    edited code / works

--- 03_integer_checker.py
+++ 03_integer_checker.py
@@ -1,24 +1,39 @@
+# checks input is a number more than a given value
+def num_check(question, low):
+    valid = False
+    while not valid:
 
-def num_check(question):
-        valid = False
-        while not valid:
+        error = "Please enter an integer that is more than (or equal to)  {}".format(low) 
 
-            error = "please enter a number that is more that zero"
+        try:
 
-            try:
+            # ask user to enter a number
+            response = int(input(question))
 
-                # ask user to enter a number
-                response = float(input(question))
+            # check number more than zero
+            if response >= low:
+                return response
 
-                # check number more than zero
-                if response > 0:
-                    return response
-
-                # outputs error if input is iinvalid
-                else:          
-                    print(error)
-                    print()
-
-            except ValueError:
+            # outputs error if input is iinvalid
+            else:          
                 print(error)
- 
+                print()
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine goes here
+
+keep_going = ""
+while keep_going == "":
+    print()
+    # ask the user for an integer (must be more than / equal to 0).
+    var_integer = num_check ("Enter an integer: ", 0)
+    print()
+
+    # ask for width & height of an image 
+    # (must be more than / equal to 1)
+    image_width = num_check ("Image width? ", 1)
+    print()
+    image_height = num_check ("Image height? ",1)
